ddec581b4913e5e69be79b18b34b18f820d686b4
Hitansu Jena <hitansu.jena@in.pega.com>
2017-03-03 14:56:30 +0530

    initial commit

--- src/testengine/Properties.java
+++ src/testengine/Properties.java
@@ -2,10 +2,10 @@ package testengine;
 
 public class Properties {
 
-	static final String USER= "root";
-	static final String PASSWORD= "pegasys1+";
-	static final String HOST= "10.150.68.171";
-	public static final String ROOT_DIR= "/dsk01/tomcat7/";
+	static final String USER= "";
+	static final String PASSWORD= "";
+	static final String HOST= "";
+	public static final String ROOT_DIR= "";
 	public static final String SINGLE_SERVER_SCRIPT= "start_stop_SINGLE.sh";
 	public static final String ALL_SERVER_SCRIPT= "start_stop_ALL.sh";
 	static final String FIVE_SERVER_SCRIPT= "";
