ca5d7d3ffa23db1421516e571475d99e6ec13c43
Hitansu Jena <hitansu.jena@in.pega.com>
2017-03-14 13:59:32 +0530

    modified

--- src/testengine/Properties.java
+++ src/testengine/Properties.java
@@ -11,20 +11,19 @@ public class Properties {
 	static final String FIVE_SERVER_SCRIPT= "";
 	static final int PORT= 22;
 	*/
-	/*user: root
-      pass: pega#1  */
+
 	
 	/*root user for the machine*/
-    static final String USER= "root";
+    static final String USER= "";
     
     /*password for root user*/
-    static final String PASSWORD= "pegasys1+";
+    static final String PASSWORD= "";
     
     /*host name*/
-    static final String HOST= "10.150.68.171";
+    static final String HOST= "";
     
     /*root directory inside which all the tomcat instance are present*/
-    public static final String ROOT_DIR= "/dsk01/tomcat7/";
+    public static final String ROOT_DIR= "";
     
     /*script to start & stop any tomcat instance*/
     public static final String SINGLE_SERVER_SCRIPT= "start_stop_SINGLE.sh";
